becefe99385bf08a33245827a50bf17837bc34b6
Collins Enebeli <techkadet@gmail.com>
2026-06-11 03:00:54 +0200

    keymidi 0.2.0 — scale chord palette

--- Casks/keymidi.rb
+++ Casks/keymidi.rb
@@ -1,9 +1,9 @@
 cask "keymidi" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.1.0"
-  sha256 arm:   "2795188f85e8d2ad136747de946123c130aca3177366356f09b463e28fcf99c6",
-         intel: "e2eab318783b45ec41f099785ac06310350bdccf546d9879d2da848d8e30f17d"
+  version "0.2.0"
+  sha256 arm:   "559d5c3c4c6646448048556d3b227e97c0363591882f4dec32fec7b37df6f309",
+         intel: "bc35602d605ee359cc0e6be168dbac1bf5f912332f94633adf7f0b2c207b8537"
 
   url "https://github.com/kadetXx/keymidi/releases/download/v#{version}/KeyMIDI-#{version}-#{arch}.dmg"
   name "KeyMIDI"
